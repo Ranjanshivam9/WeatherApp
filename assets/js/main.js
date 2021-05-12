dddaf5af569193fa76195e5de43aa8027a96b6c6
Shivam Ranjan <ranjanshivam9@gmail.com>
2021-05-12 21:05:13 +0530

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+	var lat;
+	var long;
+
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(function (position) {
+			lat = position.coords.latitude;
+			long = position.coords.longitude;
+
+			var api =
+				'https://fcc-weather-api.glitch.me/api/current?lat=' + lat + '&lon=' + long + '';
+
+			$.getJSON(api, function (res) {
+				var celsius = res.main.temp;
+				var farenheit = celsius * 1.8 + 32;
+
+				var location = res.name;
+
+				$('.weather-location').html(location);
+				$('.temp').html(Math.floor(celsius));
+				$('.weather-description').html(res.weather[0].description);
+				$('.weatherType').attr('id', res.weather[0].main);
+				$('.row2').on('click', function () {
+					if ($('.temp').html() == Math.floor(celsius)) {
+						$('.temp').html(Math.floor(farenheit));
+						$('.temp-type').html('°F');
+					}
+					else {
+						$('.temp').html(Math.floor(celsius));
+						$('.temp-type').html('°C');
+					}
+				});
+			});
+		});
+	}
+});
